abcd2c3674d16ce4cbdc74f954c25a7bb316b36e
Laura Felce <crepu83@yahoo.it>
2018-09-12 13:38:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
+return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+kittens.unshift('Bob');
+return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+var newkittens = [...kittens,'Broom'];
+ return newkittens;
+}
+
+function prependKitten(name) {
+  var kittens = ['Arnold',...kittens]
+}
